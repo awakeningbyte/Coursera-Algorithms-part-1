20357482cdb21b7bb892ccd8b00eddf09255f2f5
George Chen <gchen@awakeningbyte.com>
2018-05-20 19:05:35 -0500

    fix stylecheck

--- Assignment1_Precolation/Percolation.java
+++ Assignment1_Precolation/Percolation.java
@@ -1,113 +1,109 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
-    int _size;
-    int[] _grid;
-    int[] _status;
-    WeightedQuickUnionUF _uf;
-    boolean _percolates = false;
-    public Percolation(int n) throws java.lang.IllegalArgumentException {
+    private int size;
+    private int[] grid;
+    private int[] status;
+    private WeightedQuickUnionUF uf;
+    private boolean percolates = false;
+
+    public Percolation(int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
         }
-        _size = n;
-        _grid = new int[n*n];
-        _status = new int[n*n];
-        _uf = new WeightedQuickUnionUF(n*n);
-        for (int i=0;i<n*n; i++) {
-            _grid[i]=0;
-            _status[i]=0;
+        size = n;
+        grid = new int[n*n];
+        status = new int[n*n];
+        uf = new WeightedQuickUnionUF(n * n);
+        for (int i = 0; i < n * n; i++) {
+            grid[i] = 0;
+            status[i] = 0;
         }
     }
 
-    public void open(int row, int col) throws java.lang.IllegalArgumentException {
+    public void open(int row, int col) {
         int pos = translate(row, col);
-        //mark position open
-        if (_grid[pos] > 0) {
+        // mark position open
+        if (grid[pos] > 0) {
             return;
         }
-        _grid[pos] =1;
+        grid[pos] = 1;
 
-        //mark component status
-        if (row ==1) {
-            _status[pos] = 1;
-        } else if (row == _size ) {
-            _status[pos] = 2;
+        // mark component status
+        if (row == 1) {
+            status[pos] = 1;
+        } else if (row == size) {
+            status[pos] = 2;
         }
 
-        //check open neighours
-        int current = 0;
-
-        for(int n: neighours(row, col)) {
+        // check open neighours
+        for (int n : neighours(row, col)) {
             if (pos != n && isOpen(n)) {
-                int n_compId = _uf.find(n); 
-                int pos_compId = _uf.find(pos);
+                int nCompId = uf.find(n); 
+                int posCompId = uf.find(pos);
 
-                if ((_status[pos_compId] + _status[n_compId])==3) {
-                    this._percolates = true;
+                if ((status[posCompId] + status[nCompId]) == 3) {
+                    this.percolates = true;
                     return;
                 } 
 
-                _uf.union(pos, n);
+                uf.union(pos, n);
                 
-                if (_status[n_compId] == _status[pos_compId]) {
+                if (status[nCompId] == status[posCompId]) {
                     continue;
                 }
-                int newCompId = _uf.find(pos);
-                _status[newCompId] = Math.max(_status[pos_compId], _status[n_compId]);
-
+                int newCompId = uf.find(pos);
+                status[newCompId] = Math.max(status[posCompId], status[nCompId]);
             }
         }
     }
 
-    public boolean isOpen(int row, int col) throws java.lang.IllegalArgumentException{
+    public boolean isOpen(int row, int col) {
         return isOpen(translate(row, col));
     }
-    public boolean isOpen(int n){
-        return _grid[n]>0;
+
+    private boolean isOpen(int n) {
+        return grid[n] > 0;
     }
 
-    public boolean isFull(int row, int col) throws java.lang.IllegalArgumentException {
-        return _status[_uf.find(translate(row, col))]==1;
+    public boolean isFull(int row, int col) {
+        return status[uf.find(translate(row, col))] == 1;
     }
 
     public int numberOfOpenSites() {
-        int count =0;
-        for(int i=0;i < _size*_size; i++) {
-            count += _grid[i];
+        int count = 0;
+        for (int i = 0; i < size * size; i++) {
+            count += grid[i];
         }
         return count;
     }
     
     public boolean percolates() {
-        return _percolates;
+        return percolates;
     }
 
     public static void main(String[] args) {
 
     }
 
-    private int translate(int row, int col) throws java.lang.IllegalArgumentException {
-        if ((row < 0) || (col < 0) || (row > _size) || (col > _size)) {
+    private int translate(int row, int col) {
+        if ((row < 0) || (col < 0) || (row > size) || (col > size)) {
             throw new java.lang.IllegalArgumentException();
         }
-        return (row -1) * _size + col -1;
+        return (row -1) * size + col -1;
     }
 
     private int[] neighours(int row, int col){
-        int up = Math.max(row-1, 1);
-        int left= Math.max(col-1, 1);
-        int righ = Math.min(_size, col+1);
-        int down = Math.min(row+1, _size);
+        int up = Math.max(row - 1, 1);
+        int left = Math.max(col - 1, 1);
+        int righ = Math.min(size, col + 1);
+        int down = Math.min(row + 1, size);
 
         int[] ab = new int[4];
 
-        ab[0] = translate(up,col);
+        ab[0] = translate(up, col);
         ab[1] = translate(row, left);
-        ab[2] = translate(row, righ );
+        ab[2] = translate(row, righ);
         ab[3] = translate(down, col);
         return ab;
-        
     }
 }
--- Assignment1_Precolation/PercolationStats.java
+++ Assignment1_Precolation/PercolationStats.java
@@ -1,25 +1,24 @@
-import java.io.Console;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.Stopwatch;
+
 public class PercolationStats {
-    double[] data;
-    int _trails;
-    public PercolationStats(int n, int trails){
-        if ((n < 0) || (trails < 0)) {
+    private double[] data;
+    private int trails;
+    public PercolationStats(int n, int trials) {
+        if ((n < 0) || (trials < 0)) {
             throw new IllegalArgumentException();
         }
-        _trails = trails;
+        trails = trials;
 
-        data = new double[trails];
-        for (int i =0; i < trails; i++) {
+        data = new double[trials];
+        for (int i = 0; i < trials; i++) {
             Percolation pc = new Percolation(n);
-           while(!pc.percolates())  {
-                int row = StdRandom.uniform(1, n+1);
-                int col = StdRandom.uniform(1, n+1);
+           while (!pc.percolates())  {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
                 pc.open(row, col);
             }
-        double et = (double)pc.numberOfOpenSites()/ (n *n);
+        double et = (double)pc.numberOfOpenSites() / (n * n);
             data[i] = et;
         }
     }
@@ -31,17 +30,17 @@ public class PercolationStats {
         return StdStats.stddev(data);
     }
     public double confidenceLo() {
-        return mean()-1.96*stddev()/Math.sqrt(_trails);
+        return mean() - 1.96 * stddev() / Math.sqrt(trails);
     }
     public double confidenceHi() {
-        return mean()-1.96*stddev()/Math.sqrt(_trails);
+        return mean() - 1.96 * stddev() / Math.sqrt(trails);
     }
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int trails = Integer.parseInt( args[1]);
+        int trails = Integer.parseInt(args[1]);
         PercolationStats stats = new PercolationStats(n, trails);
-        System.out.println("mean = "+ stats.mean());
-        System.out.println("stddev ="+ stats.stddev());
-        System.out.println("95% confidnece interval = [" + stats.confidenceLo() +","+stats.confidenceHi()+"]");
+        System.out.println("mean = " + stats.mean());
+        System.out.println("stddev =" + stats.stddev());
+        System.out.println("95% confidnece interval = [" + stats.confidenceLo() + "," + stats.confidenceHi() + "]");
     }
 }
